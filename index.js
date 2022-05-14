e1557d0a0868378a97aa5dea39afdca6d531cded
Mateus Henrique <mateusdzn10@gmail.com>
2022-05-14 20:48:56 -0300

    study: trabalhando com template engine ejs

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('user');
+});
+
+app.listen(3000);
