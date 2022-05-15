8618c708a0ea5a01cf7122cdb313971227eacd90
Mateus Henrique <mateusdzn10@gmail.com>
2022-05-14 21:03:51 -0300

    feat: replacing variables

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+let userInfo = {
+    name: 'Mateus Henrique',
+    age: 17,
+};
+
 app.get('/', (req, res) => {
-    res.render('user');
+    res.render('user', userInfo);
 });
 
 app.listen(3000);
