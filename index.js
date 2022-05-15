0c07b9486d35b113c8deebcc7174885307cae62c
Mateus Henrique <mateusdzn10@gmail.com>
2022-05-14 21:09:52 -0300

    feat: using for in ejs

--- index.js
+++ index.js
@@ -5,13 +5,35 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-let userInfo = {
-    name: 'Mateus Henrique',
-    age: 17,
-};
+let usersInfo = [
+    {
+        name: 'Mateus',
+        age: 17,
+    },
+    {
+        name: 'Paula',
+        age: 25,
+    },
+    {
+        name: 'Paulo',
+        age: 28,
+    },
+    {
+        name: 'Pedro',
+        age: 16,
+    },
+    {
+        name: 'Carla',
+        age: 18,
+    },
+    {
+        name: 'Carlo',
+        age: 15,
+    },
+];
 
 app.get('/', (req, res) => {
-    res.render('user', userInfo);
+    res.render('user', { users: usersInfo });
 });
 
 app.listen(3000);
